88ad4b280197b307296735c0b3949b69169bdc68
Pravin Dahal <pravindahal@gmail.com>
2022-03-01 19:55:53 +0100

    Improve algorithm to skip certain letters

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # mocking-text
-a wEbPaGe tHaT AlLoWs yOu tO GeNeRaTe tExT LiKe tHiS.
+a WeBpAgE tHaT aLLoWs YoU to GeNeRaTe TeXt LiKe ThiS.
 
-Built in 5 minutes because I wanted to sarcastically repeat what someone said and TyPiNg lIkE ThIs iS ToO HaRd.
+Built in 5 minutes because I wanted to sarcastically repeat what someone said and TyPiNg LiKe ThiS iS too HaRd.
--- index.js
+++ index.js
@@ -1,9 +1,72 @@
 var textInput = document.getElementById("text-input");
 var mockingText = document.getElementById("mocking-text")
 
+const ALWAYS_UPPERCASE = [
+    "L", // lowercase `l` can be confused with one (`1`) or uppercase i (`I`)
+];
+
+const ALWAYS_LOWERCASE = [
+    "i", // uppercase `i` (`I`) can be confused with lowercase `L` (`l`)
+    "o", // uppercase `o` (`O`) can be confused with zero (`0`)
+];
+
+const END_OF_SENTENCE = [
+    ".",
+    "!",
+    "?",
+];
+
+Object.defineProperty(String.prototype, 'isLowerCase', {
+    value() {
+        return this.toLowerCase() == this;
+    }
+});
+
+Object.defineProperty(String.prototype, 'isUpperCase', {
+    value() {
+        return this.toUpperCase() == this;
+    }
+});
+
+function mockFunction(char, position, prev=false) {
+    if (ALWAYS_UPPERCASE.includes(char.toUpperCase())) {
+        // certain characters are always upper case
+        return char.toUpperCase();
+    }
+    if (ALWAYS_LOWERCASE.includes(char.toLowerCase())) {
+        // certain characters are always lower case
+        return char.toLowerCase();
+    }
+
+    if (position == 0 || prev == false || END_OF_SENTENCE.includes(prev)) {
+        // first character of a sentence is always lowercase
+        return char.toLowerCase();
+    }
+
+    if (prev.isLowerCase()) {
+        // return uppercase if previous non-space characters is lowercase
+        return char.toUpperCase();
+    }
+
+    if (prev.isUpperCase()) {
+        // return lowercase if previous non-space characters is uppercase
+        return char.toLowerCase();
+    }
+
+    return char;
+}
+
 function mockingConverter(str) {
-	const strArray = str.toLowerCase().split("");
-	const mocked = strArray.map((x,i) => i % 2 === 0 ? x : x.toUpperCase());
+	const strArray = str.split("");
+    const mocked = [];
+    var prev = false;
+    strArray.forEach((char, index) => {
+        var mc = mockFunction(char, index, prev);
+        mocked.push(mc);
+        if (mc != " ") {
+            prev = mc;
+        }
+    });
 	return mocked.join("");
 }
 
